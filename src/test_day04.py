f2bceadb76bcdacf36d54a163dceda8aede4854b
Nohl Childers <nohlachilders@github.com>
2024-12-05 21:15:29 -0800

    not needed to run

--- src/test_day04.py
+++ src/test_day04.py
@@ -17,7 +17,7 @@ class TestDay04(unittest.TestCase):
        #print(input_info(input))
        self.assertEqual(18, check_for_word_start(input,"XMAS"))
        #print(search_recursive(input,"XMAS",9,3,[-1,-1]))
-       print(count_x_of_mas(input))
+       #print(count_x_of_mas(input))
 
 if __name__ == "__main__":
     unittest.main()
