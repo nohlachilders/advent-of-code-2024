455da996092e8782991c10be8f211c1c61f9ccdd
Nohl Childers <nohlachilders@github.com>
2024-12-09 22:57:59 -0800

    quick test template tweak

--- src/test_template.py
+++ src/test_template.py
@@ -1,10 +1,10 @@
 import unittest
 
-from day0 import *
+#from day0 import *
 
-class TestDay0(unittest.TestCase):
-    def testDefault(self):
-        pass
+#class TestDay0(unittest.TestCase):
+#    def testDefault(self):
+#        pass
 
 if __name__ == "__main__":
     unittest.main()
